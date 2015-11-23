4a1f57035337f80f5b7477cee9e83662145c6445
Stratos Neiros <nstratos@gmail.com>
2015-11-24 00:32:51 +0200

    Add form validation for both switch states.

--- server/ui/assets/js/anisync.js
+++ server/ui/assets/js/anisync.js
@@ -27,6 +27,21 @@ var anisyncControllers = angular.module('anisyncControllers', []);
 
 anisyncControllers.controller('AnisyncCtrl', ['$scope', 'Anisync', 'ngProgressFactory',
   function($scope, Anisync, ngProgressFactory) {
+    $scope.switchToggle = function() {
+      $scope.switchOn = !$scope.switchOn;
+
+      if ($scope.switchOn == false) {
+        $scope.mainForm.$setValidity("ngRemoteValidate", undefined);
+        $scope.mainForm.malPassword.$setValidity("ngRemoteValidate", undefined);
+        $scope.mainForm.$pending = false;
+      }
+      if ($scope.switchOn == true) {
+        $scope.mainForm.malPassword.$setPristine();
+        $scope.mainForm.malPassword.$setUntouched();
+        angular.element(document.querySelector('#malPasswordInput')).val("");
+        angular.element(document.querySelector('#syncButton')).attr("disabled", "disabled");
+      }
+    };
     // Modifying ngRemoteValidate for malPassword field so that it
     // sends the username along with the password value.
     $scope.malPasswordSetArgs = function(val, el, attrs, ngModel) {
